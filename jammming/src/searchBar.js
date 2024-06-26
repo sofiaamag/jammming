ae544ba8a13e295c8305697fd18986a27aef7f92
Sofia Magnani <sofiamagnani@Sofias-MacBook-Air.local>
2024-06-26 13:54:13 -0700

    add styling

---
+++ jammming/src/searchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function SearchBar() {
+
+
+    return (
+        <div className="topnav">
+            <p>Hello</p>
+        <input type="text" placeholder="Search" />
+    
+            
+        </div>
+    )
+}
+
+export default SearchBar;
+
